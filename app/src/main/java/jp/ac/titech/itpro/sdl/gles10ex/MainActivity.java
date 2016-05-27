db2e84efb5612bac830e6b696a03c783d87bd7d9
hakaiSAN <kawai.arch.jk@gmail.com>
2016-05-27 16:35:35 +0900

    touchmove

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.SeekBar;
-
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
     private final static String TAG = "MainActivity";
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private GLSurfaceView glView;
     private SimpleRenderer renderer;
     private SeekBar rotationBarX, rotationBarY, rotationBarZ;
+
+    private GestureDetector mGestureDetector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         renderer.addObj(new Cube(0.5f, 0, 0.2f, -3));
         renderer.addObj(new Pyramid(0.5f, 0, 0, 0));
         glView.setRenderer(renderer);
+
+        mGestureDetector = new GestureDetector(this, mOnGestureListener);
     }
 
     @Override
@@ -65,5 +71,40 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
+    // タッチイベント
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
 
+    // タッチイベントのリスナー
+    private final GestureDetector.SimpleOnGestureListener mOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
+
+        // フリックイベント
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+            Log.d(TAG, "onFling");
+            try {
+                // スピードを出力
+                float velocity_x = Math.abs(velocityX);
+                float velocity_y = Math.abs(velocityY);
+                while(velocity_x > 0 || velocity_y>0) { //判定怪しい
+                    if (velocity_x > 0)
+                        Log.d(TAG, "x_moving");
+                        renderer.setRotationY(velocity_x/10);
+                    if (velocity_y > 0)
+                        Log.d(TAG, "y_moving");
+                        renderer.setRotationZ(velocity_y/10);
+
+                    velocity_x = velocity_x - 1; //改善の余地あり
+                    velocity_y = velocity_y - 1; //改善の余地あり
+                }
+
+            } catch (Exception e) {
+                // TODO
+            }
+
+            return false;
+        }
+    };
 }
